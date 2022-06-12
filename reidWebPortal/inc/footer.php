dbdebc2593272e176a59b72775934f1eab0db92d
Muhammad Wasif Ijaz <62423571+wasifijaz@users.noreply.github.com>
2022-06-12 16:23:23 +0500

    Add files via upload

---
+++ reidWebPortal/inc/footer.php
@@ -0,0 +1,8 @@
+
+
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+	  <div class="container">
+	    <p class="text-primary text-center" style="margin-top: 15px;">Copyrights &copy; <?php echo date('Y'); ?> By <strong>Re-ID</strong></p>
+	  </div><!-- /.container-fluid -->
+	</nav>
+    
---
+++ reidWebPortal/inc/header.php
@@ -0,0 +1,23 @@
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="login.php"><img src="\reidWebPortal\utils\favicon.png" style="width: 7%;position: relative;margin-bottom: -5vw; margin-top: -2vw;"></a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="index.php" style="margin-top: -0.5vw;">Home</a></li>
+	        <li><a href="logout.php" style="margin-top: -0.5vw;">Logout</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
